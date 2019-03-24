912a32de69128e3aed18968125d67edd9953bd25
karthik63 <twoindeed@gmail.com>
2019-03-24 17:00:30 +0530

    Update GAN.py

--- GAN.py
+++ GAN.py
@@ -1,4 +1,3 @@
-#-*- coding: utf-8 -*-
 from __future__ import division
 import os
 import time
@@ -7,9 +6,6 @@ from svhn_part2 import test_mnist
 # import svhn_part
 import tensorflow as tf
 import numpy as np
-
-from ops import *
-from utils import *
 
 class GAN(object):
     
@@ -155,8 +151,6 @@ class GAN(object):
             tf.nn.softmax_cross_entropy_with_logits(logits=D_fake_logits, labels=fake_labels))
         self.d_loss = d_loss_real + d_loss_fake
 
-        # get loss for generator
-        #g_loss=-log(sigmoid(D_fake_logits))等价于g_loss=-log(D(G(z))
         self.g_loss = tf.reduce_mean(
             tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=real_labels))
 
@@ -165,8 +159,6 @@ class GAN(object):
         t_vars = tf.trainable_variables()
         d_vars = [var for var in t_vars if 'discriminator/' in var.name]
         g_vars = [var for var in t_vars if 'encoder/' in var.name]
-
-        # optimizers 优化器用于减小损失函数loss，采用Adam优化器，可直接用minimize
 
 
         with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
@@ -181,12 +173,9 @@ class GAN(object):
             # self.g_optim = tf.train.AdamOptimizer(self.learning_rate * 3) \
             #           .minimize(self.g_loss, var_list=g_vars)
 
-        """" Testing """
-        # for test 由噪声生成一张图片
         self.fake_images = self.generator(self.inputs, is_training=False, reuse=True)
 
         """ Summary """
-        #Summary的含义是将参数打包后用于tensorboard的观察和模型的记录
         d_loss_real_sum = tf.summary.scalar("d_loss_real", d_loss_real)
         d_loss_fake_sum = tf.summary.scalar("d_loss_fake", d_loss_fake)
         d_loss_sum = tf.summary.scalar("d_loss", self.d_loss)
@@ -208,18 +197,7 @@ class GAN(object):
 
         self.saver = tf.train.Saver(max_to_keep=5)
 
-    #最为重要的一个函数，控制着GAN模型的训练
     def train(self):
-
-        # initialize all variables初始化各个变量
-
-        # graph inputs for visualize training results
-        #创造噪声z,GAN中应用的为均值分布，创造(64,62)大小的-1到1之间的
-        #self.sample_z = np.random.uniform(-1, 1, size=(self.batch_size, self.z_dim))
-
-        # saver to save model 将训练好的模型参数保存起来
-
-        # summary writer 将训练记录在log下
         self.writer = tf.summary.FileWriter(self.log_dir + '/' + self.model_name + datetime.datetime.now().isoformat(), self.sess.graph)
 
         # restore check-point if it exits
@@ -244,62 +222,25 @@ class GAN(object):
         start_time = time.time()
         for epoch in range(start_epoch, self.epoch):
 
-            # get batch data
-            # 由于batchsize为64，遍历70000张图片需要1093次
-
-
-
             for idx in range(start_batch_id, self.num_batches):
-                #提取处理好的固定位置图片，data_X的按批次处理后的图片位置，一个批次64张图片
                 batch_images = self.input_np[idx*self.batch_size:(idx+1)*self.batch_size]
 
-                #构造均匀分布的噪声z
                 batch_z = self.z_np[idx*self.batch_size:(idx+1)*self.batch_size].astype(np.float32)
 
-                # update D network sess.run喂入数据优化更新D网络，并在tensorboard中更新
                 _, summary_str, d_loss = self.sess.run([self.d_optim, self.d_sum, self.d_loss],
                                                feed_dict={self.inputs: batch_images, self.z: batch_z})
                 self.writer.add_summary(summary_str, counter)
 
-                # update G network sess.run喂入数据优化更新G网络，并在tensorboard中更新
                 _, summary_str, g_loss = self.sess.run([self.g_optim, self.g_sum, self.g_loss], feed_dict={self.inputs: batch_images})
                 self.writer.add_summary(summary_str, counter)
 
-                # display training status
                 counter += 1
-                #训练一个batchsize打印一下loss，一个epoch打印1093次我认为没这个必要,50次batchsize后打印一下
                 if np.mod(counter, 50) == 0:
                     print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss= %.8f, g_loss= %.8f" \
                           % (epoch, idx, self.num_batches, time.time() - start_time, d_loss, g_loss))
 
             self.save(self.checkpoint_dir, counter)
             test_mnist(self.mnist_images_test, self.mnist_labels_test, 256, self, self.sess)
-            #     # save training results for every 300 steps 训练,300步保存一张图片
-            #     # if np.mod(counter, 300) == 0:
-            #     #     #生成一张该阶段下的由生成器生成的“假图片”
-            #     #     samples = self.sess.run(self.fake_images, feed_dict={self.z: self.sample_z})
-            #     #     #此处计算生成图片的小框图片的排布，本处为8×8排布
-            #     #     tot_num_samples = min(self.sample_num, self.batch_size)
-            #     #     manifold_h = int(np.floor(np.sqrt(tot_num_samples)))
-            #     #     manifold_w = int(np.floor(np.sqrt(tot_num_samples)))
-            #     #     save_images(samples[:manifold_h * manifold_w, :, :, :], [manifold_h, manifold_w],
-            #     #                 './' + check_folder(self.result_dir + '/' + self.model_dir) + '/' + self.model_name +
-            #     #                 '_train_{:02d}_{:04d}.png'.format(epoch, idx))
-            #
-            # # After an epoch, start_batch_id is set to zero 经过一个epoch后start_batch_id置为0
-            # # non-zero value is only for the first epoch after loading pre-trained model
-            # start_batch_id = 0
-            #
-            # # save model
-
-
-            # show temporal results 经过一个epoch后输出一张全类别的图片，用于比较epoch后的差别
-            # self.visualize_results(epoch)
-
-        # save model for final step 当epoch全部训练完后保存checkpoint
-
-
-    #用于可视化epoch后输出图片
     def visualize_results(self, epoch):
         tot_num_samples = min(self.sample_num, self.batch_size)
         image_frame_dim = int(np.floor(np.sqrt(tot_num_samples)))
@@ -315,13 +256,11 @@ class GAN(object):
                     '_epoch%03d' % epoch + '_test_all_classes.png')
 
     @property
-    #加载创建固定模型下的路径，本处为GAN下的训练
     def model_dir(self):
         return "{}_{}_{}_{}".format(
             self.model_name, self.dataset_name,
             self.batch_size, self.z_dim)
 
-    #本函数的目的是在于保存训练模型后的checkpoint
     def save(self, checkpoint_dir, step):
         checkpoint_dir = os.path.join(checkpoint_dir)
 
@@ -330,7 +269,6 @@ class GAN(object):
 
         self.saver.save(self.sess,os.path.join(checkpoint_dir, self.model_name), global_step=step)
 
-    #本函数的意义在于读取训练好的模型参数的checkpoint
     def load(self, checkpoint_dir):
         import re
         print(" [*] Reading checkpoints...")
